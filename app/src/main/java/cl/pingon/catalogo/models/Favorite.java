9017690c79c4c59827f69a75e4f0e5bd4c0a91fc
nicoundu <38764619+nicoundu@users.noreply.github.com>
2018-06-06 09:40:14 -0400

    commit

--- app/src/main/java/cl/pingon/catalogo/models/Favorite.java
+++ app/src/main/java/cl/pingon/catalogo/models/Favorite.java
@@ -1,6 +1,38 @@
 package cl.pingon.catalogo.models;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import com.orm.SugarRecord;
 
 public class Favorite extends SugarRecord {
+
+    private String model;
+    private int capacity;
+
+
+    public Favorite() {
+    }
+
+    public Favorite(String model, int capacity) {
+        this.model = model;
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
 }

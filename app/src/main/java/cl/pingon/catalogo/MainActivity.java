cb15ef0d1fce0053fcfc46c10aeed49b24b00b30
nicoundu <38764619+nicoundu@users.noreply.github.com>
2018-06-06 17:11:59 -0400

    commit

--- app/src/main/java/cl/pingon/catalogo/MainActivity.java
+++ app/src/main/java/cl/pingon/catalogo/MainActivity.java
@@ -1,7 +1,11 @@
 package cl.pingon.catalogo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView favHome1 = findViewById(R.id.favHome1Tv);
+        TextView favHome2 = findViewById(R.id.favHome2Tv);
+        TextView favHome3 = findViewById(R.id.favHome3Tv);
+        TextView favHome4 = findViewById(R.id.favHome4Tv);
+        TextView favHome5 = findViewById(R.id.favHome5Tv);
+        TextView favHome6 = findViewById(R.id.favHome6Tv);
+        Button addBtn = findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addBtnIntent = new Intent(MainActivity.this, CatalogActivity.class);
+                startActivity(addBtnIntent);
+            }
+        });
+
+
+
+
+
+
     }
 }
